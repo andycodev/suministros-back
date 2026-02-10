513c4d88760e981aa2380c6cba87bfe3d5a92b3b
andyvega <andy.vega@adventistas.org>
2026-02-10 15:18:39 -0500

    error en post

--- app/Http/Controllers/Suministros/PedidosController.php
+++ app/Http/Controllers/Suministros/PedidosController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Suministros;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use App\Models\Suministros\{
     SPedido,
     SPedidoDetalle,
@@ -25,6 +26,33 @@ class PedidosController extends Controller
             'detalles.*.cantidad'    => 'required|integer|min:1',
         ]);
 
+        try {
+            $pedido = DB::transaction(function () use ($validated) {
+                $pedido = SPedido::create([
+                    'id_persona' => $validated['id_persona'],
+                    'id_destino' => $validated['id_destino'],
+                    'codigo'     => 'PE-' . strtoupper(bin2hex(random_bytes(4))),
+                    'tipo'       => $validated['tipo'],
+                    'modalidad'  => $validated['modalidad'],
+                    'total_cantidad' => 0,
+                    'total_monto'    => 0,
+                    'estado'     => 'CREADO'
+                ]);
+
+                $this->insertarDetalles($pedido, $validated['detalles']);
+
+                return $pedido->load('detalles');
+            });
+        } catch (\Throwable $e) {
+            Log::error('ERROR PEDIDOS', [
+                'message' => $e->getMessage(),
+                'trace'   => $e->getTraceAsString()
+            ]);
+
+            throw $e;
+        }
+
+        /* 
         $pedido = DB::transaction(function () use ($validated) {
 
             $pedido = SPedido::create([
@@ -41,7 +69,7 @@ class PedidosController extends Controller
             $this->insertarDetalles($pedido, $validated['detalles']);
 
             return $pedido->load('detalles');
-        });
+        }); */
 
         return response()->json($pedido, 201);
     }

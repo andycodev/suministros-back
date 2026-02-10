073d984cad105dd4cea2adec2849d1be3e058fd8
andyvega <andy.vega@adventistas.org>
2026-02-10 16:57:50 -0500

    changes corrections transactions

--- config/database.php
+++ config/database.php
@@ -96,10 +96,6 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'disable'),
-            'options' => [
-                PDO::ATTR_PERSISTENT => false, // Importante para transacciones limpias
-                PDO::ATTR_EMULATE_PREPARES => true, // Ayuda con la compatibilidad de tipos en Neon
-            ],
         ],
 
         'sqlsrv' => [
